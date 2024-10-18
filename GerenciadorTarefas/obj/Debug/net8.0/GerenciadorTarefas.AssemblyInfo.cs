3656f88d35d5e16096d66530756afa50f2ad082e
andreckw <114355564+andreckw@users.noreply.github.com>
2024-10-17 21:49:23 -0300

    ignore

--- GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
+++ GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592bf4a3221af811fda03f70b9a4d64d82071373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02074d6bc8af04e4ccb1aaf44fffc152dbc07694")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
